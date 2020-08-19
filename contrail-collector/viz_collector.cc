f4e97f5786fecd43848bf26bf0a1d1ff30432d4c
Pankaj Kumar <pankajkumar@juniper.net>
2020-08-19 14:47:22 +0530

    Collector node not created in zookeeper in case of connectivity failure
    In case of, one node receives connectivity failure then collector
    node in zookeeper gets deleted and when connectivity restores then
    collector node doesn't gets created in zookeeper because listener
    was not there to listen if connectivity restores or not.
    
    So, When connectivity restores zookeeper make a callback function
    stating that the earlier connnection was expired.
    
    So, I have created a function with all parameters which zookeeper make
    a callback and passed that function pointer to zookeeper_init so that
    if zookeeper make a callback then all events should be handled properly.
    
    closes-jira-bug: CEM-17031
    Change-Id: Ie276737330f7339b2fb7738cff47bfd3b65ae9ae

--- contrail-collector/viz_collector.cc
+++ contrail-collector/viz_collector.cc
@@ -33,6 +33,9 @@ using std::make_pair;
 using boost::system::error_code;
 using namespace zookeeper::client;
 
+static string self_ip_;
+static string zookeeper_servers_ip;
+
 VizCollector::VizCollector(EventManager *evm, unsigned short listen_port,
             const std::string &listen_ip,
             bool structured_syslog_collector_enabled,
@@ -97,11 +100,39 @@ VizCollector::VizCollector(EventManager *evm, unsigned short listen_port,
     }
 
     host_ip_ = host_ip;
+    self_ip_ = host_ip;
+    zookeeper_servers_ip = zookeeper_server_list;
+
     if (use_zookeeper) {
         std::string hostname = ResolveCanonicalName(host_ip);
         zoo_client_.reset(new ZookeeperClient(hostname.c_str(),
-            zookeeper_server_list.c_str()));
+            zookeeper_server_list.c_str()/*, NULL, false*/));
         AddNodeToZooKeeper(host_ip_);
+    }
+}
+
+void zk_watch(zhandle_t *zh, int type,
+        int state, const char *path,void *watcherCtx) {
+
+     LOG(DEBUG, "Callback from Zookeeper zk_watch called with type = " <<type<< " state = " << state);
+     string hostname = ResolveCanonicalName(self_ip_);
+     boost::scoped_ptr<zookeeper::client::ZookeeperClient> zoo_client__;
+     zoo_client__.reset(new ZookeeperClient(hostname.c_str(),
+            zookeeper_servers_ip.c_str()/*, zh, true*/));
+
+    string path1 = "/analytics-discovery-/" + g_vns_constants.COLLECTOR_DISCOVERY_SERVICE_NAME;
+    path1 += "/" + hostname;
+
+    if(state == ZOO_EXPIRED_SESSION_STATE) {
+	    //zoo_client__->Reconnect();
+    }
+
+    if (!zoo_client__->CheckNodeExist(path1.c_str())) {
+	LOG(DEBUG, "zk_watch path = " << path1 << " doesn't exist. Creating node again in Zookeeper");
+        if(state == ZOO_CONNECTED_STATE) {
+	   zoo_client__->CreateNode(path1.c_str(), hostname.c_str(),
+				Z_NODE_TYPE_EPHEMERAL);
+        }
     }
 }
 
